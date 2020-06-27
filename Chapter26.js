87e47ceebde94adcefd65c537588bb5c3ab2d7ee
Syed Muhammad Faraz Ali <57364687+Syedfaraz1997@users.noreply.github.com>
2020-06-27 13:37:23 +0500

    Add files via upload

---
+++ Chapter26.js
@@ -0,0 +1,96 @@
+//Qno-01))
+  
+    // var number = prompt("Please Enter An Positive Integer!");
+    
+    // if(number>0){
+    //     numberfunction();
+    // }else{
+    //     alert("Please Enter An Positive Number ! ");
+    // }
+
+    // function numberfunction(){
+    //     document.write("Number : " + number + "<br>");
+    //     document.write("Round Of Value : " + Math.round(number) + "<br>");
+    //     document.write("Floor Of Value: " + Math.floor(number) + "<br>");
+    //     document.write("Ceil Of Value : " + Math.ceil(number) + "<br>");
+    // }
+
+
+//Qno-02))
+
+    // var number1 = prompt("Please Enter an Negative Flooting Value : ");
+    // if(number1<0){
+    //     numberfunction1();
+    // }else{
+    //     alert("Please EnPlease Enter an Negative Flooting Value");
+    // }
+
+    // function numberfunction1(){
+    //         document.write("Number : " + number1 + "<br>");
+    //         document.write("Round Of Value : " + Math.round(number1) + "<br>");
+    //         document.write("Floor Of Value: " + Math.floor(number1) + "<br>");
+    //         document.write("Ceil Of Value : " + Math.ceil(number1) + "<br>");
+
+    // }
+
+
+//Qno03))
+
+    // var absolute = prompt("Please Enter An a Number ! ");
+    // document.write("The absolute Value of a give Number  "+ absolute + " is " + Math.abs(absolute) + "<br>");
+
+
+//Qno-04))
+
+        // var random = Math.floor(Math.random() *  6 ) +1;
+        // document.write("Random Dice value is " + random + "<br>");
+
+//Qno-05))
+
+
+        // var randomly = Math.floor(Math.random() * 2 ) +1;
+        // document.write( "Random Value is : " + randomly + "<br>");
+
+        // if(randomly === 1){
+        //     document.write(" Random Cion Value is : Heads ");
+        // }
+        // else if(randomly === 2){
+        //     document.write(" Random Cion Value is : Tails ");
+
+        // }else{
+        //     document.write("Sone Thing is Wrong  !");
+        // }
+
+
+
+        //Qno-06))
+
+
+        // var random03 = Math.floor(Math.random() * 100) +1;
+        // document.write("Random Number Between 1 to 100 is :" + random03 + "<br>");
+
+
+        //Qno-07))
+
+        // var user = prompt("Please Enter Your Accurate Weight : ");
+        // var a = parseInt(user);
+
+        // document.write("The  Weight of user is : " + a + " Kilo-gram");
+
+
+        //Qno-08))
+
+       var  randam04 = Math.floor( Math.random()  * 9) +1;  
+        
+       var  guess  = prompt("Please fint the Scret Number Between 1 to 10");
+       var b = parseInt(guess);
+
+       if(b === randam04){
+           alert("Congratulation you got a Secret Number !!!");
+       }else{
+           alert("Please Try Again :");
+       }
+
+
+
+
